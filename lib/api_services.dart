d802f092e4393a9fea8bde781462d1fc2a56f4ef
Noval Debby Prasetyono <noval.debby@gmail.com>
2019-10-12 18:19:22 +0700

    - perbaikan

--- lib/api_services.dart
+++ lib/api_services.dart
@@ -10,7 +10,7 @@ import 'package:unified_process/model/production_model.dart';
 import 'package:unified_process/model/warehouse_model.dart';
 import 'package:unified_process/helper/database_helper.dart';
 
-String api_url = "http://192.168.1.6/api/";
+String api_url = "http://192.168.1.9/api/";
 
 Future<dynamic> apiLogin(String user, String passw) async {
   try{
@@ -29,19 +29,6 @@ Future<dynamic> apiSyncWarehouse(String token, String last_update, DatabaseHelpe
   Database db = await dbHelper.database;
 
   try {
-    /*
-    final data = await db.rawQuery("SELECT * FROM warehouse WHERE warehouse_sync = 0");
-    if(data != null){
-      final response = await http.post(
-        api_url + "warehouse/sync",
-        headers: {
-          'Accept': 'application/json',
-          'Authorization': 'Bearer '+ token
-        },
-        body: {'data' : json.encode(data)}
-      );
-    }
-    */
     final response = await http.post(
       api_url + "warehouse/data",
       headers: {
@@ -55,7 +42,7 @@ Future<dynamic> apiSyncWarehouse(String token, String last_update, DatabaseHelpe
       for(int i=0; i<res.length; i++){
         WarehouseModel wh = WarehouseModel.fromDb(res[i]);
         final data = await db.rawQuery("SELECT warehouse_id FROM warehouse WHERE warehouse_id = ? ", [wh.warehouse_id]);
-        if(data != null){
+        if(data.length > 0){
           dbHelper.update(wh.tableName, 'warehouse_id', wh.toMap());
         }else{
           dbHelper.insert(wh.tableName, wh.toMap());
@@ -79,19 +66,6 @@ Future<dynamic> apiSyncArea(String token, String last_update, DatabaseHelper dbH
   Database db = await dbHelper.database;
 
   try {
-    /*
-    final data = await db.rawQuery("SELECT * FROM warehouse WHERE warehouse_sync = 0");
-    if(data != null){
-      final response = await http.post(
-        api_url + "warehouse/sync",
-        headers: {
-          'Accept': 'application/json',
-          'Authorization': 'Bearer '+ token
-        },
-        body: {'data' : json.encode(data)}
-      );
-    }
-    */
     final response = await http.post(
         api_url + "area/data",
         headers: {
@@ -105,7 +79,7 @@ Future<dynamic> apiSyncArea(String token, String last_update, DatabaseHelper dbH
       for(int i=0; i<res.length; i++){
         AreaModel datum = AreaModel.fromDb(res[i]);
         final data = await db.rawQuery("SELECT area_id FROM area WHERE area_id = ? ", [datum.area_id]);
-        if(data != null){
+        if(data.length > 0){
           dbHelper.update(datum.tableName, 'area_id', datum.toMap());
         }else{
           dbHelper.insert(datum.tableName, datum.toMap());
@@ -129,19 +103,6 @@ Future<dynamic> apiSyncLine(String token, String last_update, DatabaseHelper dbH
   Database db = await dbHelper.database;
 
   try {
-    /*
-    final data = await db.rawQuery("SELECT * FROM warehouse WHERE warehouse_sync = 0");
-    if(data != null){
-      final response = await http.post(
-        api_url + "warehouse/sync",
-        headers: {
-          'Accept': 'application/json',
-          'Authorization': 'Bearer '+ token
-        },
-        body: {'data' : json.encode(data)}
-      );
-    }
-    */
     final response = await http.post(
         api_url + "line/data",
         headers: {
@@ -155,7 +116,7 @@ Future<dynamic> apiSyncLine(String token, String last_update, DatabaseHelper dbH
       for(int i=0; i<res.length; i++){
         LineModel datum = LineModel.fromDb(res[i]);
         final data = await db.rawQuery("SELECT line_id FROM line WHERE line_id = ? ", [datum.line_id]);
-        if(data != null){
+        if(data.length > 0){
           dbHelper.update(datum.tableName, 'line_id', datum.toMap());
         }else{
           dbHelper.insert(datum.tableName, datum.toMap());
@@ -175,24 +136,10 @@ Future<dynamic> apiSyncLine(String token, String last_update, DatabaseHelper dbH
   }
 }
 
-
 Future<dynamic> apiSyncProduct(String token, String last_update, DatabaseHelper dbHelper) async {
   Database db = await dbHelper.database;
 
   try {
-    /*
-    final data = await db.rawQuery("SELECT * FROM warehouse WHERE warehouse_sync = 0");
-    if(data != null){
-      final response = await http.post(
-        api_url + "warehouse/sync",
-        headers: {
-          'Accept': 'application/json',
-          'Authorization': 'Bearer '+ token
-        },
-        body: {'data' : json.encode(data)}
-      );
-    }
-    */
     final response = await http.post(
         api_url + "product/data",
         headers: {
@@ -206,7 +153,44 @@ Future<dynamic> apiSyncProduct(String token, String last_update, DatabaseHelper 
       for(int i=0; i<res.length; i++){
         ProductModel datum = ProductModel.fromDb(res[i]);
         final data = await db.rawQuery("SELECT product_id FROM product WHERE product_id = ? ", [datum.product_id]);
-        if(data != null){
+        if(data.length > 0){
+          dbHelper.update(datum.tableName, 'product_id', datum.toMap());
+        }else{
+          dbHelper.insert(datum.tableName, datum.toMap());
+        }
+      }
+      /*
+       * Uncomment for debug
+      final data = await db.rawQuery("SELECT * FROM warehouse ORDER BY warehouse_id ASC");
+      print(data);
+       */
+    } else {
+      return '';
+    }
+  }catch(e){
+    print(e);
+    return '';
+  }
+}
+
+Future<dynamic> apiSyncProduction(String token, String last_update, DatabaseHelper dbHelper) async {
+  Database db = await dbHelper.database;
+
+  try {
+    final response = await http.post(
+        api_url + "product/data",
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer '+ token
+        },
+        body: {'last_update': last_update }
+    );
+    if (response.statusCode == 200) {
+      final res = json.decode(response.body);
+      for(int i=0; i<res.length; i++){
+        ProductModel datum = ProductModel.fromDb(res[i]);
+        final data = await db.rawQuery("SELECT product_id FROM product WHERE product_id = ? ", [datum.product_id]);
+        if(data.length > 0){
           dbHelper.update(datum.tableName, 'product_id', datum.toMap());
         }else{
           dbHelper.insert(datum.tableName, datum.toMap());
